fe529b702afed1268b8de42f5d8db2b66f49c6f0
Sudipta Sharif <sudiptasharif@gmail.com>
2019-06-23 17:48:02 -0400

    Done with chapter two.

---
+++ python_crash_course/ch2_variables_and_simple_datatypes.py
@@ -0,0 +1,67 @@
+def l(msg):
+    print(msg)
+    
+message = "Hello Python World!"
+l(message)
+
+message = "Hello Python Crash Course world!"
+l(message)
+
+message = "Hello Python Crash Course reader!"
+l(message)
+
+
+msg = "This is a string."
+l(msg)
+
+msg = 'This is also a string.'
+l(msg)
+
+msg = 'I told my friend, "Python is my favorite language"'
+l(msg)
+
+msg = "The language 'Python' is named after Monty Python, not the snake"
+l(msg)
+
+l(msg.title())
+
+l(msg.upper())
+l(msg.lower())
+
+first_name = "sudipta"
+last_name = "sharif"
+
+full_name= first_name +" "+last_name
+l(full_name.title())
+
+full_name = "\t"+first_name + '\n\t' + last_name;
+l(full_name)
+
+
+msg = " i like to day dream. "
+l(msg.rstrip())
+l(msg.lstrip())
+l(msg.strip())
+
+
+
+msg = "One of Python's strength is its diverse community."
+l(msg)
+
+msg = 'One of Python\'s strength is its diverse community'
+l(msg)
+
+age = 10**10
+msg = "Happy "+ str(age) + "rd Birthday!"
+l(msg)
+
+
+
+
+
+
+
+
+
+
+
