088da13c32ac461aa188eb72e9418aa69e53a87c
Sudipta Sharif <sudiptasharif@gmail.com>
2019-06-25 00:00:56 -0400

    Starting to learn chapter 4

---
+++ python_crash_course/ch4_working_with_lists.py
@@ -0,0 +1,63 @@
+##animals = []
+##animals.append("lion")
+##animals.append("tiger")
+##animals.append("monkey")
+##animals.append("bear")
+##animals.append("deer")
+##
+##for animal in animals:
+##    print(animal)
+##
+##magicians = ["alice", "david", "carolina"]
+##
+##for magician in magicians:
+##    print(magician.title() + ", that was a great trick")
+##print("I can't wait to see your next trick, "+magician.title()+".\n")
+##    
+##print("Thank you, everyone. That was a great magic show!")
+##
+##
+##engClubs = ["liverpool", "man city", "man united", "arsenal"]
+##engClubs.append("spurs")
+##engClubs.append("everton")
+##engClubs.append("wolves")
+##
+##print("Total length of engClubs = ",len(engClubs))
+##
+##msg = "=="*10
+##
+##for club in engClubs:
+##    msg = club.title() + " is an english premier league club." 
+##    print(msg)
+##
+##league = "English Premier League"
+##msg = "I love the "+league+"."
+##print(msg)
+##
+##league = "La Liga"
+##msg = "I love the "+league+"."
+##print(msg)
+##
+##for i in range(10):
+##    print(i)
+##
+##for i in range(1,5):
+##    print(i)
+
+
+##nums = list(range(50))
+##print(len(nums))
+##
+##nums = list(range(1000, 2000, 100))
+##print(len(nums))
+##
+##for num in nums:
+##    print(num)
+
+squares = []
+for value in range(1,11):
+    squares.append(value**2)
+print(squares);
+
+    
+
