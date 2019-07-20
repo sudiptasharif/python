854922bd0adbd70b9a90c6e42f843b169a986ef8
Sudipta Sharif <sudiptasharif@gmail.com>
2019-07-20 01:49:36 -0400

    Continuing to learn chapter 8

--- python_crash_course/chapter8/formatted_name.py
+++ python_crash_course/chapter8/formatted_name.py
@@ -7,17 +7,33 @@ def get_formatted_name(first_name, last_name, middle_name=""):
     return full_name.title()
 
 
-musician = get_formatted_name("jimi", "hendrix")
-print(musician)
+# musician = get_formatted_name("jimi", "hendrix")
+# print(musician)
+#
+# footballer = get_formatted_name("muhammad", "salah")
+# print(footballer)
+#
+# footballer = get_formatted_name("lionel", "messi")
+# print(footballer)
+#
+# musician = get_formatted_name("john", "lee", "hooker")
+# print(musician)
 
-footballer = get_formatted_name("muhammad", "salah")
-print(footballer)
+while True:
+    print("\nPlease tell me your name:")
+    print("(enter 'q' at any time to quit)")
 
-footballer = get_formatted_name("lionel", "messi")
-print(footballer)
+    first_name = input("First Name: ")
+    if first_name == 'q':
+        break
 
-musician = get_formatted_name("john", "lee", "hooker")
-print(musician)
+    last_name = input("Last Name: ")
+    if last_name == 'q':
+        break
+
+    formatted_name = get_formatted_name(first_name, last_name)
+    print("\nHello, "+formatted_name.title() + "!")
+
 
 
 
